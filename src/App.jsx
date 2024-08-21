2c9bfee33d9571975405802a35d9435d210ea38d
BarabashTV <164335010+BarabashTV@users.noreply.github.com>
2024-08-21 12:13:57 +0300

    Lisätty uusia komponentteja Header, Content, Total.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+const Header = (props) => {
+  console.log(props);
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1}: {props.exercises1}
+      </p>
+      <p>
+        {props.part2}: {props.exercises2}
+      </p>
+      <p>
+        {props.part3}: {props.exercises3}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  console.log(props);
+  return <p>Number of exercises: {props.exercises}</p>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -7,19 +33,37 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const exercises = exercises1 + exercises2 + exercises3;
+
+  const courseParts = [
+    {
+      part: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      part: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      part: "State of a component",
+      exercises: 14,
+    },
+  ];
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <hr />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <hr />
+      <Total exercises={exercises} />
     </div>
   );
 };
