36971448c3352fb74bc53003b19c8b7a2f137c8d
BarabashTV <164335010+BarabashTV@users.noreply.github.com>
2024-08-30 12:34:55 +0300

    Tehtävät 1.3-1.5 on tehty.Kurssin otsikko ja sen osat tehdään yhdeksi JavaScript-objektiksi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import Header from "./Header";
 import Content from "./Content";
-import Part from "./Part";
 import Total from "./Total";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
       <hr />
-      <Content parts={parts} />
+      <Content parts={course.parts} />
       <hr />
-      <Total parts={parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
